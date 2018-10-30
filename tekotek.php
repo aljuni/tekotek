d4f0b2cf206f8c4ee9f506e2ffd7c9e03876558d
aljuni <aljunihirossie01@gmail.com>
2018-10-30 19:41:34 +0700

    inisialisasi

--- tekotek.php
+++ tekotek.php
@@ -5,14 +5,20 @@
 </head>
 <body>
 	<?php
-		echo "<font face ='Helvetica'>Tekotek-kotek anak ayam turun 100</font> <br>";
+		echo '<p style="font-family:"Helvetica";">Tekotek-kotek anak ayam turun 100</p> <br>';
 		$tekotek = 99;
 		for ($i=$tekotek;$i > 0 ; $i--) { 
-			//echo "<font-family : Helvetica>"
-			if($i % 2 == 0)
-                {echo "<font face ='Helvetica'><font color='#964B00'>Mati satu tinggal $i</font>"; echo "<br>";}
-            else
-                { echo "<font face ='Helvetica'><font color='#FF8C00'>Mati satu tinggal $i</font>"; echo "<br>";}
+			if($i % 2 == 0){
+				echo '<p style=font-family:"Helvetica";color:brown;>'."Mati satu tinggal $i</p>";
+				echo "<br>";
+			}else{
+				echo '<p style=font-family:"Helvetica";color:orange;>'."Mati satu tinggal $i</p>";
+				echo "<br>";
+			}
+			//if($i % 2 == 0)
+              //  {echo "<font face ='Helvetica'><font color='#964B00'>Mati satu tinggal $i</font>"; echo "<br>";}
+            //else
+             //   { echo "<font face ='Helvetica'><font color='#FF8hC00'>Mati satu tinggal $i</font>"; echo "<br>";}
            
 		}
 	?>
